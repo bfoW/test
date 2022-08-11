626a18c909bc5d559c5e6c2ad1865b65893fbb65
Masumi K <bfours@gmail.com>
2022-08-11 07:37:48 +0000

    Update 13 files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import "./styles.css";
 
 document.getElementById("app").innerHTML = `
-<h1>テスト</h1>
+<h1>テスト2</h1>
 <div>
   We use the same configuration as Parcel to bundle this sandbox, you can find more
   info about Parcel 
